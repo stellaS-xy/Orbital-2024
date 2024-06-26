1f89ea224421bb6c2d573c13d5783d146d5370f1
haeri <156439185+stellaS-xy@users.noreply.github.com>
2024-06-27 00:38:10 +0900

    scene 0 done

--- Assets/Scripts/FridgeInteraction.cs
+++ Assets/Scripts/FridgeInteraction.cs
@@ -10,6 +10,13 @@ public class FridgeInteraction : MonoBehaviour
 
     public bool isPlayerInRange = false;
     public bool isInteracting = false;
+    private bool firstTimePlaying = true;
+    public GameObject arrows;
+
+
+    public string[] postVideoDialogue; // Dialogue lines to display after the video
+    private DialogueManager dialogueManager;
+
 
     private void Start()
     {
@@ -19,14 +26,21 @@ public class FridgeInteraction : MonoBehaviour
             return;
         }
 
+        arrows.SetActive(false);
         videoPlayerObject.SetActive(false); // Ensure the video player is initially disabled
         videoPlayer.loopPointReached += OnVideoFinished; // Subscribe to the event when the video finishes playing
+
+        dialogueManager = DialogueManager.Instance;
+        if (dialogueManager == null)
+        {
+            Debug.LogError("DialogueManager instance not found in the scene.");
+        }
     }
 
 
     void Update()
     {
-        if (isPlayerInRange && Input.GetKeyDown(interactKey) && !isInteracting)
+        if (isPlayerInRange && Input.GetKeyDown(interactKey) && !isInteracting && firstTimePlaying)
         {
             InteractWithFridge();
         }   
@@ -58,6 +72,7 @@ public class FridgeInteraction : MonoBehaviour
     {
         Debug.Log("Bear interacted with the fridge and video starts to play");
         isInteracting = true;
+        firstTimePlaying = false;
         videoPlayerObject.SetActive(true); // Enable the video canvas
         Debug.Log("Playing video...");
         videoPlayer.Play(); // Play the video
@@ -72,10 +87,18 @@ public class FridgeInteraction : MonoBehaviour
         Debug.Log("Video is prepared and playing.");
         videoPlayer.Play();
     }
+
     private void OnVideoFinished(VideoPlayer vp)
     {
         Debug.Log("video done playing");
         videoPlayerObject.SetActive(false); // Disable the video canvas
         isInteracting = false;
+        arrows.SetActive(true);
+
+        if (dialogueManager != null && postVideoDialogue != null && postVideoDialogue.Length > 0)
+        {
+            StartCoroutine(dialogueManager.ShowDialogue(postVideoDialogue, false));
+        }
+
     }
 }
